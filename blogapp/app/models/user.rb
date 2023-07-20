898db8abc0930c55700d2647956ab3edd3b96db1
Abdullah <abdullahkhrief@gmail.com>
2023-07-20 16:01:16 +0300

    have added the validation for the user name and user post_counter

--- blogapp/app/models/user.rb
+++ blogapp/app/models/user.rb
@@ -2,8 +2,13 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
-
+  validates :name, presence: true
+  validates :post_counter, comparison: { greater_than_or_equal_to: 0 }
+  validates :post_counter, numericality: { only_integer: true }
   def self.most_recent(input)
     User.limit(3).order(created_at: :desc).where(name: input)
   end
+# Name must not be blank.
+# PostsCounter must be an integer greater than or equal to zero.
+
 end

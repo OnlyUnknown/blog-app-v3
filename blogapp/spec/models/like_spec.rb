535d70ea0804e6ae0de735bad5e455a00777f6a9
Abdullah <abdullahkhrief@gmail.com>
2023-07-20 19:43:28 +0300

    have added tests to the user class'

--- blogapp/spec/models/like_spec.rb
+++ blogapp/spec/models/like_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  describe '#update_like_counter' do
-    before do
-      @post = Post.create!(title: 'Basket ball')
-      @like = Like.create!(author: User.create!, post: @post)
-    end
 
-    it 'should update the likes counter for a post' do
-      @like.update_like_counter
-      expect(@post.likes_counter).to eq(2)
-    end
-  end
 end

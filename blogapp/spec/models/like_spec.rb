f784a0ae3f7f835db735ccdf0c8a07cb70eafc07
Abdullah <abdullahkhrief@gmail.com>
2023-07-20 19:08:17 +0300

    have added tests to the like class

--- blogapp/spec/models/like_spec.rb
+++ blogapp/spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#update_like_counter' do
+    before do
+      @post = Post.create!(title: 'Basket ball')
+      @like = Like.create!(author: User.create!, post: @post)
+    end
+
+    it 'should update the likes counter for a post' do
+      @like.update_like_counter
+      expect(@post.likes_counter).to eq(2)
+    end
+  end
 end

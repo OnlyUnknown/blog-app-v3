095251a1aeb0e992103cc6beeb93ce8c672da420
Abdullah <abdullahkhrief@gmail.com>
2023-07-24 21:01:26 +0300

    have added the owner name for the list of the posts

--- blogapp/app/controllers/posts_controller.rb
+++ blogapp/app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def show; end
 
   def index
+    @user = User.where(id: params.require(:id))
     @posts = Post.where(author: params.require(:id))
   end
 end

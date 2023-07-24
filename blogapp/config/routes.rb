935687683e42b3f3e2e7063dade9a36efa012d0e
Abdullah <abdullahkhrief@gmail.com>
2023-07-24 19:13:26 +0300

    have added controllers to the posts and users page

--- blogapp/config/routes.rb
+++ blogapp/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/show'
+  get 'posts/index'
+  get 'users/show'
+  get 'users/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

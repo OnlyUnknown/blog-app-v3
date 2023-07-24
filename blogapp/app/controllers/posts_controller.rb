d6c47d4d9b7e56c59fff74741ccd45adcb27329d
Abdullah <abdullahkhrief@gmail.com>
2023-07-24 20:46:44 +0300

    have added route to show the specified user posts

--- blogapp/app/controllers/posts_controller.rb
+++ blogapp/app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   def show
+    
   end
 
   def index
+    @posts = Post.where(author: params.require(:id))
   end
 end
--- blogapp/config/routes.rb
+++ blogapp/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/show'
-  get 'posts/index'
+  get 'users/:id/posts', to: 'posts#index'
   get 'users/:id', to: 'users#show'
   get 'users', to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

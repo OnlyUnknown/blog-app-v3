b4e6306792614f60e5880343e6355b887f4996cd
Abdullah <abdullahkhrief@gmail.com>
2023-07-24 20:40:32 +0300

    have added the users and specified users data to their pages

--- blogapp/app/controllers/users_controller.rb
+++ blogapp/app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @users = User.where(id: params.require(:id))
   end
 
   def index
+    @users = User.all
   end
 end
--- blogapp/config/routes.rb
+++ blogapp/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'posts/show'
   get 'posts/index'
-  get 'users/show'
-  get 'users/index'
+  get 'users/:id', to: 'users#show'
+  get 'users', to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

4cd077156a2a4e217e09860c65d224e637866bc3
Abdullah <abdullahkhrief@gmail.com>
2023-07-28 16:20:56 +0300

    have the ability to add comment to specified post

--- blogapp/config/routes.rb
+++ blogapp/config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get 'users/:user_id/posts', to: 'posts#index'
   get 'users/:user_id/posts/new', to: 'posts#new'
   get 'users/:user_id/posts/:id', to: 'posts#show'
+  get 'users/:user_id/posts/:id/comment', to: 'comment#new'
   get 'users/:id', to: 'users#show'
   get 'users', to: 'users#index'
   post '/posts' => 'posts#create', :as => :create_form
+  post '/comment' => 'comment#create', :as => :create_commentF
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

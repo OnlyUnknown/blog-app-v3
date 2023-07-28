72c787f46629ac252a470493e71a3cdcebaf260c
Abdullah <abdullahkhrief@gmail.com>
2023-07-28 14:48:52 +0300

    have completed the new posts form

--- blogapp/config/routes.rb
+++ blogapp/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show'
   get 'users/:user_id/posts', to: 'posts#index'
+  get 'users/:user_id/posts/new', to: 'posts#new'
+  get 'users/:user_id/posts/:id', to: 'posts#show'
   get 'users/:id', to: 'users#show'
   get 'users', to: 'users#index'
+  post '/posts' => 'posts#create', :as => :create_form
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

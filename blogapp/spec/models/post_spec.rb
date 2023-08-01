b8b3fce16ae05bf3ab95c8a39c0f482e4269ad0a
Abdullah <abdullahkhrief@gmail.com>
2023-08-01 17:08:11 +0300

    have moved the posts_spec file to the features file and rearrangese the file

--- blogapp/spec/models/post_spec.rb
+++ blogapp/spec/models/post_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe Post, type: :model do
 
 
   before(:all) do
-user2.save
  user.save 
+ user2.save
  post.save 
  post2.save 
  post3.save 
@@ -27,9 +27,7 @@ after(:all) do
   User.destroy_all
   Comment.destroy_all
 end
-  
-  before { post.save }
-  before { user.save }
+
   it 'title should be present' do
     post.title = nil
     expect(post).to_not be_valid
@@ -38,6 +36,7 @@ end
   it 'title should be less than 250 length' do
     post.title = 'a' * 251
     expect(post).to_not be_valid
+    post.title = "Cars"
   end
 
   it 'comments_counter should be integer' do
@@ -48,6 +47,7 @@ end
   it 'comments_counter should be greater than or equal zero' do
     post.comments_counter = -1
     expect(post).to_not be_valid
+    post.comments_counter = 1
   end
 
   it 'liks_counter should be integer' do
@@ -58,50 +58,14 @@ end
   it 'likes_counter should be greater than or equal zero' do
     post.likes_counter = -1
     expect(post).to_not be_valid
+    post.likes_counter = 20
   end
 
   describe 'method' do
     it 'should update the posts_counter of the user' do
       post.update_posts_counter(user)
-      expect(user.posts_counter).to eq(1)
-    end
-    it 'should retrun the name of the most recent' do
-      view = post.most_recent_post(comment)
-      expect(view).to eq('Cars')
+      expect(user.posts_counter).to eq(5)
     end
   end
 
-  describe 'Post view', :type => :feature do
-    describe 'post index' do
-      before{visit "/users/1/posts"}
-    scenario 'shows post and direction to them' do
-      sleep(3)
-      expect(page).to have_css("img[src*='/assets/xpfs.jpg']")
-      expect(page).to have_content('Tom')
-      expect(page).to have_content("Number of posts: 4")
-      expect(page).to have_content("Cars")
-      expect(page).to have_content("text of first post")
-      expect(page).to have_content("Lilly : text")
-      expect(page).to have_content("comment counter: 1")
-      expect(page).to have_content("likes counter:20")
-      click_link("POST#1")
-      expect(page).to have_content("text of first post")
-      sleep(3)
-      expect(page).to have_content("Tom")
-    end
-  end
-
-  describe 'post show' do
-    before{visit "/users/1/posts/1"}
-    scenario 'showing the specefied post' do
-        sleep(3)
-        expect(page).to have_content('by Tom')
-        expect(page).to have_content("Cars")
-      expect(page).to have_content('Tom')
-      expect(page).to have_content("comment counter: 1")
-      expect(page).to have_content("likes counter:20")
-      expect(page).to have_content("text of first post")
-      expect(page).to have_content("Lilly : text")
-      expect(page).to have_content("Tom : text")
-  end
 end
